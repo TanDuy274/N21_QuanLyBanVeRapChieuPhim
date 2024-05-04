91f74de30f69746a3290a1a5b8a187467c5acf35
Tan Nguyen Hoang <hoangtan22th@gmail.com>
2024-05-04 18:10:21 +0700

    giao dien quan ly hoa don

--- src/dao/HoaDon_DAO.java
+++ src/dao/HoaDon_DAO.java
@@ -1,6 +1,7 @@
 package dao;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -34,4 +35,88 @@ public class HoaDon_DAO {
 		}
 		return dsHoaDon;
 	}
+	
+	public ArrayList<HoaDon> getAllHoaDonWithDetails() {
+        ArrayList<HoaDon> dsHoaDon = new ArrayList<>();
+        try {
+            ConnectDB.getIntance();
+            Connection con = ConnectDB.getConnection();
+            String sql = "SELECT hd.*, nv.*, kh.* " +
+                         "FROM HoaDon hd " +
+                         "INNER JOIN NhanVien nv ON hd.maNhanVien = nv.maNhanVien " +
+                         "INNER JOIN KhachHang kh ON hd.maKhachHang = kh.maKhachHang";
+            Statement statement = con.createStatement();
+            ResultSet rs = statement.executeQuery(sql);
+            while (rs.next()) {
+                String maHD = rs.getString("maHoaDon");
+                Date ngapLapHD = rs.getDate("ngayLapHD");
+                NhanVien nv = new NhanVien(rs.getString("maNhanVien"));
+                nv.setTenNhanVien(rs.getString("tenNhanVien")); 
+                KhachHang kh = new KhachHang(rs.getString("maKhachHang"));
+                kh.setTenKhachHang(rs.getString("tenKhachHang")); 
+                HoaDon hd = new HoaDon(maHD, ngapLapHD, kh, nv);
+                dsHoaDon.add(hd);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return dsHoaDon;
+    }
+	public ArrayList<HoaDon> getHoaDonByDate(String fromDate, String toDate) {
+	    ArrayList<HoaDon> dsHoaDon = new ArrayList<>();
+	    try {
+	        ConnectDB.getIntance();
+	        Connection con = ConnectDB.getConnection();
+	        String sql = "SELECT hd.*, nv.*, kh.* " +
+	                     "FROM HoaDon hd " +
+	                     "INNER JOIN NhanVien nv ON hd.maNhanVien = nv.maNhanVien " +
+	                     "INNER JOIN KhachHang kh ON hd.maKhachHang = kh.maKhachHang " +
+	                     "WHERE hd.ngayLapHD >= '" + fromDate + "' AND hd.ngayLapHD <= '" + toDate + "'";
+	        Statement statement = con.createStatement();
+	        ResultSet rs = statement.executeQuery(sql);
+	        while (rs.next()) {
+	            String maHD = rs.getString("maHoaDon");
+	            Date ngapLapHD = rs.getDate("ngayLapHD");
+	            NhanVien nv = new NhanVien(rs.getString("maNhanVien"));
+	            nv.setTenNhanVien(rs.getString("tenNhanVien")); 
+	            KhachHang kh = new KhachHang(rs.getString("maKhachHang"));
+	            kh.setTenKhachHang(rs.getString("tenKhachHang")); 
+	            HoaDon hd = new HoaDon(maHD, ngapLapHD, kh, nv);
+	            dsHoaDon.add(hd);
+	        }
+	    } catch (SQLException e) {
+	        e.printStackTrace();
+	    }
+	    return dsHoaDon;
+	}
+	public ArrayList<HoaDon> getHoaDonByMaWithDetails(String maHoaDon) {
+	    ArrayList<HoaDon> dsHoaDon = new ArrayList<>();
+	    try {
+	        ConnectDB.getIntance();
+	        Connection con = ConnectDB.getConnection();
+	        String sql = "SELECT hd.*, nv.*, kh.* " +
+	                     "FROM HoaDon hd " +
+	                     "INNER JOIN NhanVien nv ON hd.maNhanVien = nv.maNhanVien " +
+	                     "INNER JOIN KhachHang kh ON hd.maKhachHang = kh.maKhachHang " +
+	                     "WHERE hd.maHoaDon = ?";
+	        PreparedStatement statement = con.prepareStatement(sql);
+	        statement.setString(1, maHoaDon);
+	        ResultSet rs = statement.executeQuery();
+	        while (rs.next()) {
+	            Date ngayLapHD = rs.getDate("ngayLapHD");
+	            NhanVien nv = new NhanVien(rs.getString("maNhanVien"));
+	            nv.setTenNhanVien(rs.getString("tenNhanVien")); 
+	            KhachHang kh = new KhachHang(rs.getString("maKhachHang"));
+	            kh.setTenKhachHang(rs.getString("tenKhachHang")); 
+	            HoaDon hd = new HoaDon(maHoaDon, ngayLapHD, kh, nv);
+	            dsHoaDon.add(hd);
+	        }
+	    } catch (SQLException e) {
+	        e.printStackTrace();
+	    }
+	    return dsHoaDon;
+	}
+
+
+
 }
